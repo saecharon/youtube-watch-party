0eaa122ff129aa545be3a2778cc77c74a3a0297a
saecharon <saicharan2939@gmail.com>
2026-08-09 17:47:16 +0530

    Upgrade Zynlivo brand and logo

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-self.__WATCH_PARTY_CACHE = "zynlivo-pwa-v27";
+self.__WATCH_PARTY_CACHE = "zynlivo-pwa-v28";
 self.__WATCH_PARTY_ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=zynlivo-simple-11",
-  "/app.js?v=zynlivo-simple-11",
+  "/styles.css?v=zynlivo-simple-12",
+  "/app.js?v=zynlivo-simple-12",
   "/manifest.webmanifest",
   "/icons/icon.svg",
   "/icons/icon-192.png",
